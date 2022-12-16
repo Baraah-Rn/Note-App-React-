f6659694b61753bd52d25c2adec36d8bbe9c31af
Baraah-Rn <baraahranneh@gmail.com>
2022-12-16 15:01:32 +0100

    Create the layout and App components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import NotesList from './components/NotesList';
 
-function App() {
+const App=()=> {
+  const[notes,setNotes]= useState([
+    {
+    id:uuidv4 (),
+    text : "text1",
+    date: "12/12/2012"
+  },  {
+    id: uuidv4 (),
+    text : "text2",
+    date: "12/12/2012"
+  },  {
+    id: uuidv4 (),
+    text : "text3",
+    date: "12/12/2012"
+  },  {
+    id: uuidv4 (),
+    text : "text4",
+    date: "12/12/2012"
+  },
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <NotesList notes={notes}/>
     </div>
   );
 }
