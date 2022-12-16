a1401637deb02de278a50517a76254f07b15e987
Baraah-Rn <baraahranneh@gmail.com>
2022-12-16 15:53:47 +0100

    Add delete function

--- src/App.js
+++ src/App.js
@@ -35,10 +35,14 @@ const newNotes = [...notes, newNote]
 setNotes(newNotes)
 }
 
+const deleteNote = (id) =>{
+  const newNotes = notes.filter((note)=>note.id !== id)
+setNotes(newNotes)
+}
 
   return (
     <div className='container'>
-      <NotesList notes={notes} handleAddNote={addNote}/>
+      <NotesList notes={notes} handleAddNote={addNote} handleDeleteNote ={deleteNote}/>
     </div>
   );
 }
